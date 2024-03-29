ec24edea90aac5d42924bb37b931e7f4f91e8af4
Yongjun Park <yopark@student.42seoul.kr>
2024-03-29 18:36:38 +0900

    build(origin): add BACKEND_ORIGIN on production

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,12 +1,14 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
 import { HTTP_HEADERS } from '@/constants/http-headers';
+import { BACKEND_PROXY_ORIGIN } from '@/constants/url';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:10241',
+  baseURL: BACKEND_PROXY_ORIGIN,
 });
 
 const axiosRequestInterceptorConfig = (config: InternalAxiosRequestConfig) => {
+  config.headers[HTTP_HEADERS.CONTENT_TYPE_KEY] = 'application/json';
   config.headers[HTTP_HEADERS.CLIENT_NAME_KEY] = 'Yongjun Park';
 
   return config;

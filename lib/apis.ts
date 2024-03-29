e5f7d311e0d15b608c07a1a84f902ca24775de1d
Yongjun Park <yopark@student.42seoul.kr>
2024-03-29 13:55:04 +0900

    init: axios response types

--- lib/apis.ts
+++ lib/apis.ts
@@ -1,24 +1,41 @@
 import { axiosInstance } from '@/lib/axios';
 import { AxiosPatchTodoDto, AxiosPostTodoDto } from '@/types/dto';
+import {
+  AxiosDeleteTodoOfResponseDto,
+  AxiosGetTodoOfResponseDto,
+  AxiosGetTodosResponseDto,
+  AxiosPatchTodoOfResponseDto,
+  AxiosPostTodoResponseDto,
+} from '@/types/response-dto';
 
 const axiosPostTodo = (axiosPostTodoDto: AxiosPostTodoDto) => {
-  return axiosInstance.post(BACKEND_ROUTES.TODOS, axiosPostTodoDto);
+  return axiosInstance.post<AxiosPostTodoResponseDto>(
+    BACKEND_ROUTES.TODOS,
+    axiosPostTodoDto,
+  );
 };
 
 const axiosGetTodos = () => {
-  return axiosInstance.get(BACKEND_ROUTES.TODOS);
+  return axiosInstance.get<AxiosGetTodosResponseDto>(BACKEND_ROUTES.TODOS);
 };
 
 const axiosGetTodoOf = (id: number) => {
-  return axiosInstance.get(BACKEND_ROUTES.TODO_OF(id));
+  return axiosInstance.get<AxiosGetTodoOfResponseDto>(
+    BACKEND_ROUTES.TODO_OF(id),
+  );
 };
 
 const axiosPatchTodoOf = (id: number, axiosPatchTodoDto: AxiosPatchTodoDto) => {
-  return axiosInstance.patch(BACKEND_ROUTES.TODO_OF(id), axiosPatchTodoDto);
+  return axiosInstance.patch<AxiosPatchTodoOfResponseDto>(
+    BACKEND_ROUTES.TODO_OF(id),
+    axiosPatchTodoDto,
+  );
 };
 
 const axiosDeleteTodoOf = (id: number) => {
-  return axiosInstance.delete(BACKEND_ROUTES.TODO_OF(id));
+  return axiosInstance.delete<AxiosDeleteTodoOfResponseDto>(
+    BACKEND_ROUTES.TODO_OF(id),
+  );
 };
 
 export {

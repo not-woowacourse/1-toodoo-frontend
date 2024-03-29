bdda368f6f5a9a366440c791a6e533ca20d13e03
Yongjun Park <yopark@student.42seoul.kr>
2024-03-29 17:43:57 +0900

    feat(create-todo-action): logic

---
+++ features/root/create-todo-sheet.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { ChangeEventHandler, ReactNode, useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { QUERY_KEYS } from '@/constants/query-keys';
+import { axiosPostTodo } from '@/lib/apis';
+
+type CreateTodoSheetProps = {
+  trigger: ReactNode;
+};
+
+const CreateTodoSheet = ({ trigger }: CreateTodoSheetProps) => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: axiosPostTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.TODOS });
+
+      toast.success('할 일을 추가했어요.');
+    },
+    onError: () => {
+      toast.error('할 일 추가에 실패했어요. 잠시 후에 시도해주세요.');
+    },
+  });
+
+  const [title, setTitle] = useState<string>('');
+
+  const isTitleEmpty = title === '';
+
+  const handleTitleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { value } = event.currentTarget;
+
+    setTitle(value);
+  };
+
+  const handleSubmit = () => {
+    mutate({ title });
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{trigger}</SheetTrigger>
+      <SheetContent side="bottom">
+        <SheetHeader>
+          <SheetTitle>New Todo</SheetTitle>
+          <SheetDescription>Create a new reminder.</SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="title" className="text-right">
+              Title
+            </Label>
+            <Input
+              id="title"
+              value={title}
+              onChange={handleTitleChange}
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button disabled={isTitleEmpty} onClick={handleSubmit}>
+              Create
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default CreateTodoSheet;
--- features/root/todo-list-item-update-action.tsx
+++ features/root/todo-list-item-update-action.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Todo } from '@/types/todo';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -15,11 +14,15 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet';
 import TodoListItemUpdateButton from '@/features/root/todo-list-item-update-button';
+import { Todo } from '@/types/todo';
+
 type TodoListItemUpdateActionProps = {
   todo: Todo;
 };
 
 const TodoListItemUpdateAction = ({ todo }: TodoListItemUpdateActionProps) => {
+  const { title, description } = todo;
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -37,19 +40,17 @@ const TodoListItemUpdateAction = ({ todo }: TodoListItemUpdateActionProps) => {
             <Label htmlFor="title" className="text-right">
               Title
             </Label>
-            <Input id="title" value="Pedro Duarte" className="col-span-3" />
+            <Input id="title" value={title} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description" className="text-right">
               Description
             </Label>
-            <Input id="description" value="@peduarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="priority" className="text-right">
-              Priority
-            </Label>
-            <Input id="priority" value="@peduarte" className="col-span-3" />
+            <Input
+              id="description"
+              value={description ?? ''}
+              className="col-span-3"
+            />
           </div>
         </div>
         <SheetFooter>

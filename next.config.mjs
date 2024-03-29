b11235753ebbc16a513cbb659bb9a86e74293232
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-03-29 22:48:55 +0900

    fix: CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    // const BACKEND_ORIGIN =
+    //   (process.env.NODE_ENV === 'development' && 'http://localhost:10241') ||
+    //   (process.env.NODE_ENV === 'production' &&
+    //     'https://port-0-my-tiny-backend-am952lltl4h1s4.sel5.cloudtype.app') ||
+    //   '';
+
+    const BACKEND_ORIGIN =
+      'https://port-0-my-tiny-backend-am952lltl4h1s4.sel5.cloudtype.app';
+
+    return [
+      {
+        source: '/backend-api/:path*',
+        destination: `${BACKEND_ORIGIN}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;

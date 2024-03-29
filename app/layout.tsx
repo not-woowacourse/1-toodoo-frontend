d6e39a056b306e15f2143ad836a82096f3cee963
Yongjun Park <yopark@student.42seoul.kr>
2024-03-29 14:02:55 +0900

    init(react-query): query-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { PropsWithChildren } from 'react';
+
+import QueryProvider from '@/providers/query-provider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,7 +15,9 @@ const metadata: Metadata = {
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="ko-KR">
-      <body className={inter.className}>{children}</body>
+      <QueryProvider>
+        <body className={inter.className}>{children}</body>
+      </QueryProvider>
     </html>
   );
 };

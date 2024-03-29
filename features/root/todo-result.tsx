06269dd8360986a45aab7ae82c041d8904fd0504
Yongjun Park <yopark@student.42seoul.kr>
2024-03-29 17:22:20 +0900

    feat(show-already-done-toggle): logic

--- features/root/todo-result.tsx
+++ features/root/todo-result.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useState } from 'react';
+
+import ShowAlreadyDoneToggle from '@/features/root/show-already-done-toggle';
 import TodoList from '@/features/root/todo-list';
 import { Todo } from '@/types/todo';
 
@@ -6,13 +11,29 @@ type TodoResultProps = {
 };
 
 const TodoResult = ({ result }: TodoResultProps) => {
+  const [isShowAlreadyDone, setIsAlreadyDone] = useState<boolean>(false);
+
+  const handlePressedChange = (pressed: boolean) => {
+    setIsAlreadyDone(pressed);
+  };
+
+  if (!isShowAlreadyDone) {
+    result = result.filter((todo) => !todo.isDone);
+  }
+
   const { length } = result;
 
   return (
     <div className="flex flex-col gap-1">
-      <p className="text-sm text-neutral-500">
-        총 {length}개의 할 일이 있습니다.
-      </p>
+      <div className="flex justify-between">
+        <p className="text-sm text-neutral-500">
+          총 {length}개의 할 일이 있습니다.
+        </p>
+        <ShowAlreadyDoneToggle
+          defaultPressed={isShowAlreadyDone}
+          onPressedChange={handlePressedChange}
+        />
+      </div>
       <TodoList todos={result} />
     </div>
   );

d660d5dbdb7a2f2edc0f23503c82cb0a8e7e4874
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-03-30 20:25:40 +0900

    feat: delete todos

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,14 @@ const RootPage = () => {
   const sorted = data?.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
 
   return (
-    <main className="flex min-h-screen w-screen flex-col items-center justify-center p-4">
-      <h1 className="mb-8 text-3xl font-bold text-neutral-800">
-        My To-Do List
+    <main className="flex min-h-screen w-screen flex-col items-center p-4 sm:p-8">
+      <h1 className="w-full max-w-xl break-keep pb-4 pl-3.5 text-3xl font-bold text-neutral-800 sm:pb-8">
+        할 일
       </h1>
-      {!data || isLoading ? <p>Loading...</p> : null}
-      {sorted && (
+      {isLoading ? <p>Loading...</p> : null}
+      {!sorted && !isLoading ? <p>에러</p> : null}
+      {sorted && sorted.length === 0 ? <p>텅</p> : null}
+      {sorted && sorted.length > 0 && (
         <ul className="flex w-full max-w-xl flex-col gap-3">
           {sorted.map((item) => (
             <TodoCard todo={item} key={item.id} />
--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,4 +1,4 @@
-import { AlertCircle, Check, PencilLine, Trash2 } from 'lucide-react';
+import { AlertCircle, Check, Loader2, PencilLine, Trash2 } from 'lucide-react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
@@ -16,9 +16,8 @@ export function TodoCard({
 
   const {
     mutate: toggle,
-    isPending,
-    error,
-    variables,
+    isPending: isToggling,
+    error: toggleError,
   } = useMutation({
     mutationFn: () =>
       axios.patch<TodoPatchResponse>(
@@ -31,24 +30,46 @@ export function TodoCard({
     },
   });
 
+  const {
+    mutate: erase,
+    isPending: isErasing,
+    error: eraseError,
+  } = useMutation({
+    mutationFn: () =>
+      axios.delete(`${API_URL}/todos/${id}`, { headers: DEFAULT_HEADERS }),
+    onSettled: async () => {
+      return await queryClient.invalidateQueries({ queryKey: ['todos'] });
+    },
+  });
+
+  const isLoading = isToggling || isErasing;
+
   return (
-    <li className="group flex flex-wrap rounded-xl border border-neutral-200 bg-white shadow-sm transition-all has-[:disabled]:cursor-not-allowed [&:has(label:active):not(:has(:disabled))]:active:scale-[0.975] [&:has(label:hover)]:border-neutral-300 [&:has(label:hover)]:bg-neutral-100">
-      <label className="flex flex-grow cursor-pointer gap-2.5 p-3.5">
+    <li className="group flex flex-wrap rounded-xl border border-neutral-200 bg-white shadow-sm transition-all [&:has(label:active):not(:has(:disabled))]:active:scale-[0.975] [&:has(label:hover):not(:has(:disabled))]:border-neutral-300 [&:has(label:hover):not(:has(:disabled))]:bg-neutral-100">
+      <label className="flex flex-grow cursor-pointer gap-2.5 p-3.5 has-[:disabled]:cursor-not-allowed">
         <div className="sticky top-4 flex size-6 flex-none">
           <input
             type="checkbox"
-            className="peer size-full cursor-pointer appearance-none rounded border border-neutral-200 bg-white transition-all checked:border-neutral-900 checked:bg-neutral-800 disabled:opacity-40"
+            className="peer size-full cursor-pointer appearance-none rounded border border-neutral-200 bg-white transition-all checked:border-neutral-900 checked:bg-neutral-800 disabled:cursor-not-allowed disabled:opacity-50"
             checked={isDone}
-            disabled={isPending}
+            disabled={isLoading}
             onChange={() => toggle()}
           />
-          <Check
-            className="text-netural-900 absolute left-1 top-1 opacity-0 transition-all peer-checked:text-white peer-checked:opacity-100 peer-disabled:opacity-40"
-            size={16}
-            strokeWidth={4}
-          />
+          {isToggling ? (
+            <Loader2
+              className="text-netural-900 absolute left-1 top-1 animate-spin opacity-0 transition-all peer-checked:text-white peer-checked:opacity-100 peer-disabled:opacity-50"
+              size={16}
+              strokeWidth={4}
+            />
+          ) : (
+            <Check
+              className="text-netural-900 absolute left-1 top-1 opacity-0 transition-all peer-checked:text-white peer-checked:opacity-100 peer-disabled:opacity-50"
+              size={16}
+              strokeWidth={4}
+            />
+          )}
         </div>
-        <div className="flex flex-grow flex-col gap-1 text-balance break-keep pr-1">
+        <div className="flex flex-grow flex-col gap-1 break-keep">
           <h2 className="font-medium text-neutral-900 decoration-transparent transition-colors group-has-[:checked]:text-neutral-400 group-has-[:checked]:line-through group-has-[:checked]:decoration-neutral-900">
             {title}
           </h2>
@@ -57,24 +78,46 @@ export function TodoCard({
               {description}
             </p>
           )}
-          {error && (
-            <div
-              role="alert"
-              className="mt-2 flex w-full items-center gap-2 rounded-lg bg-red-100 p-2.5 text-sm font-semibold text-red-600"
-            >
-              <AlertCircle />
-              <span>{error.message}</span>
-            </div>
-          )}
         </div>
       </label>
-      <div className="ml-auto flex flex-none gap-0.5 p-2.5">
-        <button className="flex size-8 items-center justify-center rounded text-neutral-600 transition-all hover:bg-neutral-200 active:scale-95">
+      <div className="group ml-auto flex flex-none gap-0.5 p-2.5 pl-0">
+        <button
+          disabled={isLoading}
+          className="flex size-8 items-center justify-center rounded text-neutral-600 transition-all active:scale-95 disabled:cursor-not-allowed disabled:opacity-50 [&:not(:disabled)]:hover:bg-neutral-200"
+        >
           <PencilLine size={20} />
         </button>
-        <button className="flex size-8 items-center justify-center rounded text-neutral-600 transition-all hover:bg-neutral-200 active:scale-95">
-          <Trash2 size={20} />
+        <button
+          disabled={isLoading}
+          onClick={() => erase()}
+          className="flex size-8 items-center justify-center rounded text-neutral-600 transition-all active:scale-95 disabled:cursor-not-allowed disabled:opacity-50 [&:not(:disabled)]:hover:bg-neutral-200"
+        >
+          {isErasing ? (
+            <Loader2 size={20} className="animate-spin" />
+          ) : (
+            <Trash2 size={20} />
+          )}
         </button>
+      </div>
+      <div className="w-full p-3.5 pt-0 empty:hidden">
+        {toggleError && (
+          <div
+            role="alert"
+            className="flex w-full items-center gap-2 rounded-lg bg-red-100 p-2.5 text-sm font-semibold text-red-600"
+          >
+            <AlertCircle />
+            <span>{toggleError.message}</span>
+          </div>
+        )}
+        {eraseError && (
+          <div
+            role="alert"
+            className="flex w-full items-center gap-2 rounded-lg bg-red-100 p-2.5 text-sm font-semibold text-red-600"
+          >
+            <AlertCircle />
+            <span>{eraseError.message}</span>
+          </div>
+        )}
       </div>
     </li>
   );

175bd2da837733196685d9d7535570ec6cffde68
smreosms13 <smreosms13@gamil.com>
2024-04-04 04:05:52 +0900

    design: align texts in TodoInfo

--- components/atoms/TodoInfo.tsx
+++ components/atoms/TodoInfo.tsx
@@ -5,14 +5,14 @@ type TodoInfoProps = {
 
 const TodoInfo = ({ title, description }: TodoInfoProps) => {
   return (
-    <>
+    <div className="flex flex-col gap-1 flex-1">
       <p className="text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
         {title}
       </p>
       {description && (
-        <p className="text-sm text-muted-foreground">{description}</p>
+        <p className="text-xs text-muted-foreground">{description}</p>
       )}
-    </>
+    </div>
   );
 };
 

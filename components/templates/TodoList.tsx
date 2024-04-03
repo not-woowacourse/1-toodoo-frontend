496de9107480b8cb5ee00a520aaf4a860092df42
smreosms13 <smreosms13@gamil.com>
2024-04-03 23:24:32 +0900

    feat: add TodoList component

--- components/templates/TodoList.tsx
+++ components/templates/TodoList.tsx
@@ -1,2 +1,38 @@
-// todo list
-// 생성, 삭제, 수정 시 업데이트
+'use client';
+import { useQuery } from '@tanstack/react-query';
+
+import { apiGetTodos } from '@/lib/apis';
+import { TodoItem } from '@/components/organs/TodoItem';
+
+const TodoList = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['todos'],
+    queryFn: apiGetTodos,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  if (!data) {
+    return <div>No data</div>;
+  }
+
+  const todos = data.data;
+
+  return (
+    <div className="flex flex-col gap-1">
+      <ul className="flex flex-col gap-2">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} hideCompleted={false} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;

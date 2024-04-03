092473b807b496c3bfe6ebe82aa625b507c67914
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-04-03 23:23:20 +0900

    refactor: fetch client name from environment variables

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const API_URL = 'https://not-woowacourse-api.yopark.dev';
 
-export const CLIENT_NAME = 'chanhwi';
+export const CLIENT_NAME = process.env.NEXT_PUBLIC_CLIENT_NAME;
 
 export const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',

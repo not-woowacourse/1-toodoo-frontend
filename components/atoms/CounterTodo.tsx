fc3cd282c736cc591d6e263209079c695e502bd4
smreosms13 <smreosms13@gamil.com>
2024-04-05 15:40:04 +0900

    design: set heigth of CounterTodo

--- components/atoms/CounterTodo.tsx
+++ components/atoms/CounterTodo.tsx
@@ -8,7 +8,7 @@ const CounterTodo = () => {
   let doneLength = todos.length - unDoneTodos.length;
 
   return (
-    <div className="flex flex-col justify-end gap-1 flex-1">
+    <div className="h-10 flex flex-col justify-end gap-1 flex-1">
       <p className="text-sm">총 {unDoneTodos.length}개의 할 일</p>
       {isShowDone && <p className="text-sm">총 {doneLength}개의 할 일 완료</p>}
     </div>

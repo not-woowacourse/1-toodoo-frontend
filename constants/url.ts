078b455cb14cdcca5f4c703ff7c00f66b7c7ba97
Yongjun Park <yopark@student.42seoul.kr>
2024-04-05 13:04:02 +0900

    chore(cors): change FRONTEND_URL to newly deployed url

--- constants/url.ts
+++ constants/url.ts
@@ -1,7 +1,7 @@
 const FRONTEND_URL =
   (process.env.NODE_ENV === 'development' && 'http://localhost:10242') ||
   (process.env.NODE_ENV === 'production' &&
-    'https://not-woowacourse-1-toodoo-frontend-for-example.vercel.app') ||
+    'https://not-woowacourse-toodoo.yopark.dev') ||
   '';
 
 const API_PROXY_URL = new URL('/backend-api', FRONTEND_URL).toString();

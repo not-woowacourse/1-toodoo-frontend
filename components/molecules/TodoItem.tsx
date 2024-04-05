145c66a90d6e4ff0e08442797926b870dbc8e1f9
smreosms13 <smreosms13@gamil.com>
2024-04-05 15:33:04 +0900

    feat: hide completed TodoItem if isShowDone true

--- components/molecules/TodoItem.tsx
+++ components/molecules/TodoItem.tsx
@@ -1,6 +1,7 @@
-'use client';
+import { useContext } from 'react';
 
 import { Todo } from '@/types/types';
+import { TodoContext } from '@/store/todo-context';
 import CompleteTodoCheckbox from '@/components/atoms/CompleteTodoCheckbox';
 import TodoInfo from '@/components/atoms/TodoInfo';
 import DeleteTodoButton from '@/components/atoms/DeleteTodoButton';
@@ -12,7 +13,13 @@ type TodoItemProps = {
 
 export const TodoItem = ({ todo }: TodoItemProps) => {
   const { id, title, description, isDone } = todo;
+  const { isShowDone } = useContext(TodoContext);
 
+  let itemHidden = isDone && !isShowDone;
+
+  if (itemHidden) {
+    return null;
+  }
   return (
     <li className="w-full h-14 p-3 flex items-center gap-3 bg-white rounded-md">
       <CompleteTodoCheckbox todoId={id} isDone={isDone} />

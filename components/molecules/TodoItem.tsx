83008f832a4deab4fd57be84c19caa85f3cc6783
smreosms13 <smreosms13@gamil.com>
2024-04-05 15:55:13 +0900

    design: Add border and shadow to TodoItem li

--- components/molecules/TodoItem.tsx
+++ components/molecules/TodoItem.tsx
@@ -21,7 +21,7 @@ export const TodoItem = ({ todo }: TodoItemProps) => {
     return null;
   }
   return (
-    <li className="w-full h-14 p-3 flex items-center gap-3 bg-white rounded-md">
+    <li className="w-full h-14 p-3 flex items-center gap-3 bg-white rounded-md border border-neutral-200 hover:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
       <CompleteTodoCheckbox todoId={id} isDone={isDone} />
       <TodoInfo title={title} description={description} />
       <UpdateTodoSheet todo={todo} />

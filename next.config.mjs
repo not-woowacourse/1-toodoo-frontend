4485cfb1e5cf3401c5578d3c724a093c508f4a07
김유나 <yuna000804@gmail.com>
2024-04-05 17:01:17 +0900

    fix: rewrites backend origin 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    // const BACKEND_ORIGIN =
-    //   (process.env.NODE_ENV === 'development' && 'http://localhost:10241') ||
-    //   (process.env.NODE_ENV === 'production' &&
-    //     'https://port-0-my-tiny-backend-am952lltl4h1s4.sel5.cloudtype.app') ||
-    //   '';
-
-    const BACKEND_ORIGIN =
-      'https://port-0-my-tiny-backend-am952lltl4h1s4.sel5.cloudtype.app';
+    const BACKEND_ORIGIN = 'https://not-woowacourse-api.yopark.dev';
 
     return [
       {
